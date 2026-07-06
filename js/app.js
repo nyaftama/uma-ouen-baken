a78d06907b59cc4db6447e3a7cee1436d124b034
nyaftama <nyaftama@gmail.com>
2026-07-06 21:59:47 +0900

    Adjust the ticket design

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 
-const version = document.getElementById('version') ? document.getElementById('version').textContent : '1.06c';
+const version = document.getElementById('version') ? document.getElementById('version').textContent : '1.06d';
 const umaCsvPath = './data/uma_list.csv?v=' + version;
 
 let allUmaData = [];

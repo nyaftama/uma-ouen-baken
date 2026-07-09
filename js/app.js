e8ac7f8338db08faa4d5571568181723049d926e
nyaftama <nyaftama@gmail.com>
2026-07-09 11:19:38 +0900

    Update color palette to solid black

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 
-const version = document.getElementById('version') ? document.getElementById('version').textContent : '1.07a';
+const version = document.getElementById('version') ? document.getElementById('version').textContent : '1.07b';
 const umaCsvPath = './data/uma_list.csv?v=' + version;
 
 let allUmaData = [];

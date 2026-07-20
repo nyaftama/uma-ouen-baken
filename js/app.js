c9ff2302e906bee950a80dec6c4af532c5a0b762
nyaftama <nyaftama@gmail.com>
2026-07-20 20:57:54 +0900

    Update character list data

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 
-const version = document.getElementById('version') ? document.getElementById('version').textContent : '1.08e';
+const version = document.getElementById('version') ? document.getElementById('version').textContent : '1.08f';
 const umaCsvPath = './data/uma_list.csv?v=' + version;
 
 let allUmaData = [];

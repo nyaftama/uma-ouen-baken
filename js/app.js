8daf135612f071c412a98243860265f0e9ac66c7
nyaftama <nyaftama@gmail.com>
2026-07-21 12:47:59 +0900

    Update horse list data source

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 
-const version = document.getElementById('version') ? document.getElementById('version').textContent : '1.08h';
+const version = document.getElementById('version') ? document.getElementById('version').textContent : '1.09';
 const umaCsvPath = './data/uma_list.csv?v=' + version;
 
 let allUmaData = [];

9724ce33b153a3d4de7d5e2f1da095addb3a02e6
nyaftama <nyaftama@gmail.com>
2026-07-22 00:20:07 +0900

    Add underline to credit link text

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 
-const version = document.getElementById('version') ? document.getElementById('version').textContent : '1.09';
+const version = document.getElementById('version') ? document.getElementById('version').textContent : '1.09a';
 const umaCsvPath = './data/uma_list.csv?v=' + version;
 
 let allUmaData = [];

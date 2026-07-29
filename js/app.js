5abcbd850779b0aae228a353a0f070b2979a1817
nyaftama <ctrsflvr@googlemail.com>
2026-07-29 18:21:58 +0900

    Update race data list

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 
-const version = document.getElementById('version') ? document.getElementById('version').textContent : '1.09b';
+const version = document.getElementById('version') ? document.getElementById('version').textContent : '1.09c';
 const umaCsvPath = './data/uma_list.csv?v=' + version;
 
 let allUmaData = [];
